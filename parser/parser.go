e5e3712912671afe832cd7549f6fa1a4b4b718f2
Aleksander Mistewicz <a.mistewicz@gmail.com>
2020-01-28 12:48:59 +0100

    Use strings instead of byte slices

--- parser/parser.go
+++ parser/parser.go
@@ -2,9 +2,9 @@ package parser
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/awesomepatrol/bb-match-history/stats"
 )
@@ -13,42 +13,42 @@ func ParseSingleMatch(reader io.Reader) (*stats.Match, error) {
 	scanner := bufio.NewScanner(reader)
 	match := new(stats.Match)
 	for scanner.Scan() {
-		line := scanner.Bytes()
+		line := scanner.Text()
 		// TODO: check if bold
 		switch {
-		case bytes.HasPrefix(line, []byte("Status:")):
-		case bytes.HasSuffix(line, []byte("has joined the game")):
+		case strings.HasPrefix(line, "Status:"):
+		case strings.HasSuffix(line, "has joined the game"):
 			event := new(stats.Event)
 			event.EventType = stats.JoinGame
-			_, err := fmt.Sscanf(string(line), "%s has joined the game", &event.Payload)
+			_, err := fmt.Sscanf(line, "%s has joined the game", &event.Payload)
 			if err != nil {
 				break
 			}
 			match.Timeline = append(match.Timeline, event)
-		case bytes.HasSuffix(line, []byte("has left the game")):
+		case strings.HasSuffix(line, "has left the game"):
 			event := new(stats.Event)
 			event.EventType = stats.LeaveGame
-			_, err := fmt.Sscanf(string(line), "%s has left the game", &event.Payload)
+			_, err := fmt.Sscanf(line, "%s has left the game", &event.Payload)
 			if err != nil {
 				break
 			}
 			match.Timeline = append(match.Timeline, event)
-		case bytes.HasSuffix(line, []byte("has joined team north!")):
+		case strings.HasSuffix(line, "has joined team north!"):
 			player := new(stats.Player)
-			_, err := fmt.Sscanf(string(line), "%s has joined team north!", &player.Name)
+			_, err := fmt.Sscanf(line, "%s has joined team north!", &player.Name)
 			if err != nil {
 				break
 			}
 			match.North.Players = append(match.North.Players, player)
-			match.Timeline = append(match.Timeline, &stats.Event{EventType: stats.JoinTeam, Payload: string(line)})
-		case bytes.HasSuffix(line, []byte("has joined team south!")):
+			match.Timeline = append(match.Timeline, &stats.Event{EventType: stats.JoinTeam, Payload: line})
+		case strings.HasSuffix(line, "has joined team south!"):
 			player := new(stats.Player)
-			_, err := fmt.Sscanf(string(line), "%s has joined team south!", &player.Name)
+			_, err := fmt.Sscanf(line, "%s has joined team south!", &player.Name)
 			if err != nil {
 				break
 			}
 			match.South.Players = append(match.South.Players, player)
-			match.Timeline = append(match.Timeline, &stats.Event{EventType: stats.JoinTeam, Payload: string(line)})
+			match.Timeline = append(match.Timeline, &stats.Event{EventType: stats.JoinTeam, Payload: line})
 		}
 	}
 	if err := scanner.Err(); err != nil {
