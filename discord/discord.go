a42ec20758c2eeafc5244d44dcc64bd55f820521
Aleksander Mistewicz <a.mistewicz@gmail.com>
2020-01-29 18:54:16 +0100

    Slightly refactor discord bot

--- discord/discord.go
+++ discord/discord.go
@@ -11,13 +11,17 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+const masterID = "213745524279345152"
+
 var (
-	match *stats.Match
-	bot   *discordgo.Session
+	match         *stats.Match
+	bot           *discordgo.Session
+	validChannels map[string]interface{}
 )
 
 func init() {
 	match = parser.NewMatch()
+	validChannels = make(map[string]interface{})
 }
 
 func OpenBot(token string) {
@@ -39,32 +43,77 @@ func CloseBot() {
 	bot.Close()
 }
 
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-	if m.Author.ID == s.State.User.ID {
+func sendReplyInDM(s *discordgo.Session, recipientID string, content string) {
+	ch, err := s.UserChannelCreate(recipientID)
+	if err != nil {
+		log.Println(err)
 		return
 	}
+	_, err = s.ChannelMessageSend(ch.ID, content)
+	if err != nil {
+		log.Println(err)
+	}
+}
 
-	log.Println(m.Content)
-
+func processMatchMessages(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Process map restart
 	if m.Content == `**\*\*\* Map is restarting! \*\*\***` {
 		parser.FixPlayers(match)
 		stats.InsertMatch(match)
+		match = parser.NewMatch()
 
 		ret, err := json.Marshal(match)
 		if err != nil {
 			log.Println(err)
+			return
 		}
 		_, err = s.ChannelMessageSend(m.ChannelID, string(ret))
 		if err != nil {
 			log.Println(err)
 		}
-		match = parser.NewMatch()
 		return
 	}
 
+	// Process bold messages
 	if strings.HasPrefix(m.Content, "**") {
 		line := strings.Trim(m.Content, "*")
 		log.Println("parsing:", line)
 		parser.ParseLine(match, line)
+		return
+	}
+
+	// Process embed messages
+	for _, e := range m.Embeds {
+		line := e.Description
+		log.Println("parsing:", line)
+		parser.ParseLine(match, line)
+	}
+}
+
+func processMasterCommands(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Content == `!test` {
+		sendReplyInDM(s, m.Author.ID, "ok")
+	}
+	if m.Content == `!addChannel` {
+		validChannels[m.ChannelID] = nil
+		sendReplyInDM(s, m.Author.ID, "channel "+m.ChannelID+" added")
+	}
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// Ignore own messages (shouldn't happen often anyway)
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	// Process only added channels
+	if _, ok := validChannels[m.ChannelID]; ok {
+		log.Println(*m.Message)
+		processMatchMessages(s, m)
+	}
+
+	// Commands for master only
+	if m.Author.ID == masterID {
+		processMasterCommands(s, m)
 	}
 }
