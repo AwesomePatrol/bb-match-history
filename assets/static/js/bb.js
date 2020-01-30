704bbdcee1f8d0384c92b527c523b1505629f1da
Aleksander Mistewicz <a.mistewicz@gmail.com>
2020-01-30 12:51:31 +0100

    Remove coloring from recent matches

--- assets/static/js/bb.js
+++ assets/static/js/bb.js
@@ -15,11 +15,18 @@ function addRecentMatchesEntry(tbl, match) {
       winner = "North";
     }
     let diffStr = diff2str[match.Difficulty];
-    tbl.append($("<tr>")
+    let row = $("<tr>")
       .append($("<td>").append(match.ID))
       .append($("<td>").append(ago.fromNow()))
       .append($("<td>").append(match.Length))
       .append($("<td>").append(winner))
-      .append($("<td>").append(diffStr))
-      );
+      .append($("<td>").append(diffStr));
+    if (!(typeof match.IsWinner === 'undefined')) {
+        if (match.IsWinner) {
+            row.addClass("table-success")
+        } else {
+            row.addClass("table-danger")
+        }
+    }
+    tbl.append(row);
 }
