c474f15b952f3c2bbf5e34d4b4623b51ad616649
Aleksander Mistewicz <a.mistewicz@gmail.com>
2020-12-28 17:42:33 +0100

    Add comments to discord functions

--- discord/discord.go
+++ discord/discord.go
@@ -55,6 +55,7 @@ func GetCurrentTournament() *stats.Match {
 	return _getCurrentMatch(tournamentServer)
 }
 
+// NewMatch initializes match structure and sets it
 func NewMatch(chanID string) (m *stats.Match) {
 	m = parser.NewMatch()
 	m.ChannelID = chanID
@@ -62,6 +63,8 @@ func NewMatch(chanID string) (m *stats.Match) {
 	return
 }
 
+// OpenBot connects to discord with given token.
+// It will panic if any error occurs.
 func OpenBot(token string) {
 	var err error
 	bot, err = discordgo.New("Bot " + token)
@@ -79,10 +82,12 @@ func OpenBot(token string) {
 	go parseCurrent(bot, casualServer, time.Now().AddDate(0, 0, -1))
 }
 
+// CloseBot disconnects from discord.
 func CloseBot() {
 	bot.Close()
 }
 
+// sendReplyInDM sends message directly instead of posting it to a channel.
 func sendReplyInDM(s *discordgo.Session, recipientID string, content string) {
 	log.Println("reply in DM:", content)
 	if recipientID == "" {
@@ -98,11 +103,15 @@ func sendReplyInDM(s *discordgo.Session, recipientID string, content string) {
 		log.Println(err)
 	}
 }
+
+// processMatchMessages retreives timestamp from message for _processMatchMessages.
 func processMatchMessages(s *discordgo.Session, m *discordgo.Message, match *stats.Match, skip bool) bool {
 	t, _ := discordgo.SnowflakeTimestamp(m.ID)
 	return _processMatchMessages(s, m, match, t, skip)
 }
 
+// processMatchEnd fills missing values and fixes common issues in match structure.
+// If skip is set to true, the match isn't put into the database.
 func processMatchEnd(match *stats.Match, t time.Time, skip bool) bool {
 	log.Println("GAME RESTART")
 	parser.FixPlayers(match)
@@ -125,11 +134,15 @@ func processMatchEnd(match *stats.Match, t time.Time, skip bool) bool {
 	return true
 }
 
+// _processMatchMessages parses a single discord message m for match assuming time t.
+// If skip is set to true, the match isn't put into the database.
 func _processMatchMessages(s *discordgo.Session, m *discordgo.Message, match *stats.Match, t time.Time, skip bool) bool {
 	for _, line := range strings.Split(m.Content, "\n") {
+		// Skip extremely short lines
 		if len(line) < 4 {
 			continue
 		}
+
 		// Process map restart
 		if strings.Contains(line, "Map is restarting") || strings.Contains(line, "Server is shutting down") {
 			return processMatchEnd(match, t, skip)
@@ -160,6 +173,8 @@ func _processMatchMessages(s *discordgo.Session, m *discordgo.Message, match *st
 	return false
 }
 
+// getRelevantHistory reads all messages in chanID that are newer than time t and returns them as
+// lines. If current is set to true, it will stop on the first map restart.
 func getRelevantHistory(s *discordgo.Session, chanID string, t time.Time, current bool) (lines []*discordgo.Message) {
 	//TODO: filter by Author.ID
 	ch, err := s.Channel(chanID)
@@ -190,17 +205,17 @@ func getRelevantHistory(s *discordgo.Session, chanID string, t time.Time, curren
 			if msg.Author.ID == s.State.User.ID { // Ignore own
 				continue
 			}
-			if strings.HasPrefix(msg.Content, "**") || len(msg.Embeds) > 0 {
-				if current && strings.Contains(msg.Content, "Map is restarting") {
-					return
-				}
-				lines = append(lines, msg)
+			if current && strings.Contains(msg.Content, "Map is restarting") {
+				return
 			}
+			lines = append(lines, msg)
 		}
 	}
 	return
 }
 
+// parseHistory reads all messages in chanID that are newer than time t and then inserts all parsed
+// matches in the database.
 func parseHistory(s *discordgo.Session, chanID string, t time.Time) {
 	lines := getRelevantHistory(s, chanID, t, false)
 	historyMatch := parser.NewMatch()
@@ -215,6 +230,8 @@ func parseHistory(s *discordgo.Session, chanID string, t time.Time) {
 	}
 }
 
+// parseCurrent reads messages in chanID until the first match start (but no older than time t) and
+// then processes it into currentMatch.
 func parseCurrent(s *discordgo.Session, chanID string, t time.Time) {
 	lines := getRelevantHistory(s, chanID, t, true)
 	mux.Lock()
@@ -227,6 +244,8 @@ func parseCurrent(s *discordgo.Session, chanID string, t time.Time) {
 	}
 }
 
+// scanChannels lists channels on all guilds and adds them to watched lists when they match a
+// pattern. authorID is informed when a matching channels is found.
 func scanChannels(s *discordgo.Session, authorID string) {
 	for _, guild := range s.State.Guilds {
 		channels, err := s.GuildChannels(guild.ID)
@@ -252,6 +271,7 @@ func scanChannels(s *discordgo.Session, authorID string) {
 	sendReplyInDM(s, authorID, "Done.")
 }
 
+// processMasterCommands acts on messages from masterID.
 func processMasterCommands(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Content == `!test` {
 		sendReplyInDM(s, m.Author.ID, "ok")
@@ -296,6 +316,7 @@ func processMasterCommands(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 }
 
+// messageCreate is a handler for new messages that bot receives.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	// Ignore own messages (shouldn't happen often anyway)
 	if m.Author.ID == s.State.User.ID {
@@ -307,9 +328,11 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	// Process only added channels
 	if _, ok := currentMatch[m.ChannelID]; ok {
+		// Process only trusted authors (bots)
 		if _, ok := trustedBotIDs[m.Author.ID]; ok {
 			log.Println(*m.Message, m.Author.ID)
 			if processMatchMessages(s, m.Message, currentMatch[m.ChannelID], false) {
+				// Match ended so a new one should be created
 				NewMatch(m.ChannelID)
 			}
 		}
