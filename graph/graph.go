2ba09e033af3ca5175af42abd722c268fad8f6f1
Aleksander Mistewicz <a.mistewicz@gmail.com>
2021-01-25 18:42:14 +0100

    Configure RenderPlayerELO to look better

--- graph/graph.go
+++ graph/graph.go
@@ -90,20 +90,29 @@ func RenderDifficultyBreakdown(w io.Writer, after time.Time) error {
 }
 
 func RenderPlayerELO(matches []*stats.GamePlayer, w io.Writer) error {
-	x := make([]time.Time, len(matches))
-	y := make([]float64, len(matches))
+	if len(matches) == 0 {
+		return fmt.Errorf("no matches to render")
+	}
+	x := make([]time.Time, len(matches)+1)
+	y := make([]float64, len(matches)+1)
+	x[0] = matches[0].Match.Start
+	y[0] = float64(matches[0].BeforeELO)
 	for i, m := range matches {
-		x[i] = m.Match.End
-		y[i] = float64(m.BeforeELO + m.GainELO)
+		x[i+1] = m.Match.End
+		y[i+1] = float64(m.BeforeELO + m.GainELO)
 	}
 	mainSeries := chart.TimeSeries{
+		Style: chart.Style{
+			StrokeColor: chart.ColorAlternateGray,
+			StrokeWidth: 3.0,
+		},
 		XValues: x,
 		YValues: y,
 	}
 	minSeries := &chart.MinSeries{
 		Style: chart.Style{
 			StrokeColor:     chart.ColorAlternateGray,
-			StrokeDashArray: []float64{5.0, 5.0},
+			StrokeDashArray: []float64{4.0, 4.0},
 		},
 		InnerSeries: mainSeries,
 	}
@@ -111,15 +120,34 @@ func RenderPlayerELO(matches []*stats.GamePlayer, w io.Writer) error {
 	maxSeries := &chart.MaxSeries{
 		Style: chart.Style{
 			StrokeColor:     chart.ColorAlternateGray,
-			StrokeDashArray: []float64{5.0, 5.0},
+			StrokeDashArray: []float64{4.0, 4.0},
 		},
 		InnerSeries: mainSeries,
 	}
 	graph := chart.Chart{
-		Height: 120,
-		Width:  480,
+		Height: 300,
+		Width:  1000,
 		XAxis: chart.XAxis{
-			TickPosition: chart.TickPositionBetweenTicks,
+			ValueFormatter: func(v interface{}) string {
+				if typed, isTyped := v.(time.Time); isTyped {
+					return typed.Format("Jan 2")
+				}
+				if typed, isTyped := v.(float64); isTyped {
+					return time.Unix(0, int64(typed)).Format("Jan 2")
+				}
+				return "---"
+			},
+			GridMajorStyle: chart.Style{
+				StrokeColor: chart.ColorAlternateGray,
+				StrokeWidth: 1.0,
+			},
+			GridMinorStyle: chart.Style{
+				StrokeColor: chart.ColorLightGray,
+				StrokeWidth: 1.0,
+			},
+		},
+		YAxis: chart.YAxis{
+			ValueFormatter: chart.IntValueFormatter,
 		},
 		Series: []chart.Series{
 			mainSeries,
